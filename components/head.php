9e7d56eb561d342ce9bdb5fa2f39a55e80e89149
yuseisui <53548640+yuseisui@users.noreply.github.com>
2020-03-02 00:58:04 +0900

    追加: head コンポーネント

---
+++ components/head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title><?php if (isset($title)) echo "$title - "; ?>てんこっこ</title>
+<link rel="stylesheet" href="/styles/reset.min.css">
+<link rel="stylesheet" href="/styles/_variables.css">
+<link rel="stylesheet" href="/styles/global.css">
