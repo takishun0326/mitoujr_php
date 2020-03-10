01b4387cd213d22ecc6d968dcbb3f86cf3df7f2a
yuseisui <53548640+yuseisui@users.noreply.github.com>
2020-03-11 03:53:21 +0900

    hotfix

--- tenko/RollCall/complete-RollCall.php
+++ tenko/RollCall/complete-RollCall.php
@@ -1,3 +1,55 @@
+<?php
+include("../../pdo.php");
+include("../../kotai-shikibetsu-number.php");
+
+$tenkoFinishCheck = $pdo->prepare("SELECT * FROM memberlist WHERE kotaiNum = ?");
+$tenkoFinishCheck->execute([$mobile_id]);
+
+$tenkoFinished = "false";
+
+foreach ($tenkoFinishCheck->fetchAll() as $row) {
+  if ($row["RollCallCheck"] == 0) {
+    $tenkoFinished = "false";
+  } else {
+    $tenkoFinished = "true";
+    $RollCallTimes = $row["RollCallCount"] + 1;
+  }
+}
+
+if ($tenkoFinished == "false") {
+  // RollCallCheck -> 1
+  $rollCallCheck = $pdo->prepare("UPDATE memberlist
+        SET RollCallCheck = :RollCallCheck
+         WHERE id =
+         (SELECT id FROM
+           (SELECT id FROM memberlist WHERE kotaiNum = :kotaiNum)
+              as tmp)");
+  $params1       = array(
+    ':RollCallCheck' => '1',
+    ':kotaiNum' => $mobile_id
+  );
+  $rollCallCheck->execute($params1);
+}
+
+// RollCallCount ++
+$rollCallCount1 = $pdo->prepare("UPDATE memberlist
+      SET RollCallCount =
+        (SELECT RollCallCount + 1 FROM
+  		      (SELECT RollCallCount FROM
+              (SELECT * FROM memberlist WHERE kotaiNum = :kotaiNum1)
+            as tmp1)
+        as tmp2)
+      WHERE id =
+        (SELECT id FROM
+          (SELECT id FROM memberlist WHERE kotaiNum = :kotaiNum2)
+      as tmp3)");
+$params2        = array(
+  ':kotaiNum1' => $mobile_id,
+  ':kotaiNum2' => $mobile_id
+);
+$rollCallCount1->execute($params2);
+?>
+
 <!-- View -->
 <?php
 $title = '点呼完了';
